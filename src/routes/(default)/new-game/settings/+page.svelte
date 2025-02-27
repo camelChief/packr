ea490486d72a912c13a2d4e14af39aabec9ef245
Caleb Campbell <ccampbell@biscit.com.au>
2025-02-28 09:20:06 +1100

    Cleaned.

--- src/routes/(default)/new-game/settings/+page.svelte
+++ src/routes/(default)/new-game/settings/+page.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import { goto } from '$app/navigation';
-	import { SETTINGS } from '$lib/constants';
-	import type { Setting } from '$lib/models/setting';
-	import { eventStore } from '$lib/stores';
-	import { onDestroy, onMount } from 'svelte';
-	import type { Unsubscriber } from 'svelte/store';
+	import { goto } from "$app/navigation";
+	import { SETTINGS } from "$lib/constants";
+	import type { Setting } from "$lib/models/settings";
+	import { eventStore } from "$lib/stores";
+	import { onDestroy, onMount } from "svelte";
+	import type { Unsubscriber } from "svelte/store";
 
-	interface EditableSetting extends Setting {
-		value: boolean | number | string;
-	}
+    interface EditableSetting extends Setting {
+        value: boolean | number | string;
+    }
 
-	let unsubscribeFromEvents: Unsubscriber;
-	let settings: EditableSetting[] = $state([]);
+    let unsubscribeFromEvents: Unsubscriber;
+    let settings: EditableSetting[] = $state([]);
 
-	function subscribeToEvents() {
+    function subscribeToEvents() {
 		unsubscribeFromEvents = eventStore.subscribe(async (event) => {
 			if (event === 'nextPage') {
 				eventStore.set('');
@@ -36,36 +36,36 @@
 		});
 	}
 
-	function populateSettings() {
-		settings = SETTINGS.map((setting) => ({ ...setting, value: setting.default }));
-	}
+    function populateSettings() {
+        settings = SETTINGS.map(setting => ({ ...setting, value: setting.default }));
+    }
 
-	onMount(() => {
-		subscribeToEvents();
-		populateSettings();
-	});
+    onMount(() => {
+        subscribeToEvents();
+        populateSettings();
+    });
 
-	onDestroy(() => unsubscribeFromEvents());
+    onDestroy(() => unsubscribeFromEvents());
 </script>
 
 <main>
-	<p class="mb-8">Apply settings here which will only affect the current game.</p>
+    <p class="mb-8">Apply settings here which will only affect the current game.</p>
 
-	<ul class="list">
-		{#each settings as setting}
-			<li class="list-row items-center">
-				{#if setting.type === 'boolean'}
-					<!-- <div class="w-12"> -->
-					<input type="checkbox" checked={setting.value as boolean} class="toggle" />
-					<!-- </div> -->
-				{:else if setting.type === 'number'}
-					<input type="number" value={setting.value as number} class="input w-10" />
-				{/if}
-				<div>
-					<div>{setting.label}</div>
-					<div class="text-base-content/50 text-base">{setting.description}</div>
-				</div>
-			</li>
-		{/each}
-	</ul>
+    <ul class="list">
+        {#each settings as setting}
+            <li class="list-row items-center">
+                {#if setting.type === 'boolean'}
+                    <!-- <div class="w-12"> -->
+                        <input type="checkbox" checked={setting.value as boolean} class="toggle" />
+                    <!-- </div> -->
+                {:else if setting.type === 'number'}
+                    <input type="number" value={setting.value as number} class="input w-10" />
+                {/if}
+                <div>
+                    <div>{setting.label}</div>
+                    <div class="text-base-content/50 text-base">{setting.description}</div>
+                </div>
+            </li>
+        {/each}
+    </ul>
 </main>
