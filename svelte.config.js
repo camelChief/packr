7c09aa93caec4765e8f565471e70e3f5db8a74fb
Caleb Campbell <ccampbell@biscit.com.au>
2025-03-02 08:41:21 +1100

    Modified deploy script, added base path for prod deployments.

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 	kit: {
-		adapter: adapter({
-			fallback: '404.html'
-		})
+		adapter: adapter({ fallback: '404.html' }),
+		paths: { base: isProduction ? '/packr' : '' }
 	}
 };
 
